881809462036ef8af55b244f93b80db3cf4ca70d
sanjay-simform <sanjay.nagar@simformsolutions.com>
2026-06-03 14:02:53 +0530

    fix: sqllite fixes

--- backend/src/db/data-source.ts
+++ backend/src/db/data-source.ts
@@ -4,7 +4,7 @@ import { DataSource } from "typeorm";
 export const AppDataSource = new DataSource({
   type: "sqlite",
   database: "db.db",
-  // host: ENV.DB_HOST,
+  // host: ENV.DB_HOST,qqqq
   // port: ENV.DB_PORT,
   // username: ENV.DB_USER,
   // password: ENV.DB_PASSWORD,
